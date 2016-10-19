2314a2c06ae532104d1e4cd0af624ccb4b72f406
Justin <jproche5@gmail.com>
2016-10-19 16:12:28 -0400

    test

--- client/src/app.js
+++ client/src/app.js
@@ -242,7 +242,7 @@ app.initialBoard = function(){
     'o o o o '.split(''),
     ' o o o o'.split(''),
     '        '.split(''),
-    '        '.split(''),
+    '   o    '.split(''),
     'x x x x '.split(''),
     ' x x x x'.split(''),
     'x x x x '.split(''),
